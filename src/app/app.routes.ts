ac44da23fd27013fdb2bfb3d4b06d2243c0bbaf3
Lubangula <danielmayombo47@gmail.com>
2025-05-18 07:35:07 +0200

    add folder docs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,7 +32,7 @@ export const routes: Routes = [
     component: CreateComponent
   },
   {
-    path: 'articles/edit/:id',
+    path: 'articles/edit/:id',  
     component: EditComponent
   },
   {
